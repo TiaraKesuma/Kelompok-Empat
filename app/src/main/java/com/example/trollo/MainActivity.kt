1290e6d1da8b7c17e7289ef619430c307a089a67
Aditya Cahyo <aditydcp@gmail.com>
2022-11-16 00:01:35 +0700

    refactor to allow fragment use

--- app/src/main/java/com/example/trollo/MainActivity.kt
+++ app/src/main/java/com/example/trollo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.trollo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Lifecycle Callback: onCreate")
     }
 }
