0261bafde221cc60c53148051912fffb2d2f4054
Aditya Cahyo <aditydcp@gmail.com>
2022-11-30 04:41:18 +0700

    restructure directory

--- app/src/main/java/com/example/trollo/ui/addTask/AddActivity.kt
+++ app/src/main/java/com/example/trollo/ui/addTask/AddActivity.kt
@@ -1,7 +1,8 @@
-package com.example.trollo
+package com.example.trollo.ui.addTask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trollo.R
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/trollo/ui/mainView/MainActivity.kt
+++ app/src/main/java/com/example/trollo/ui/mainView/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.trollo
+package com.example.trollo.ui.mainView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trollo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
